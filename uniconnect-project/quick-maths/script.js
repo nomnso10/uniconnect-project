12d3df54a438c0beb04a003274993a5a39308516
nomnso10 <trenchardchinomnso@gmail.com>
2026-04-07 17:10:33 +0100

    Delete uniconnect-project directory

--- uniconnect-project/quick-maths/script.js
+++
@@ -1,8 +0,0 @@
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
-const result = document.getElementById("result");
-const calcBtn = document.getElementById("calc-btn");
-
-calcBtn.addEventListener("click", function () {
-  result.textContent = Number(num1.value) + Number(num2.value);
-});
